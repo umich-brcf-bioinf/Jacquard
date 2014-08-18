07d71db8d825e4662c99d89c19a938d1a1f90ce5
jebene <jebene@umich.edu>
2014-08-18 13:27:14 -0400

    reverted

--- bin/merge.py
+++ bin/merge.py
@@ -404,26 +404,6 @@ def create_new_line(alt_allele_number, fields):
 
     return "\t".join(new_line) + "\n"
     
-def determine_caller(reader, unknown_callers):
-    caller = "unknown"
-    mutect_dict = {}
-    for line in reader:
-        if line.startswith("##jacquard.tag.caller="):
-            caller = line.split("=")[1].strip("\n")
-        elif line.startswith("##MuTect="):
-            split_line = line.split(" ")
-            for item in split_line:
-                split_item = item.split("=")
-                try:
-                    mutect_dict[split_item[0]] = split_item[1]
-                except:
-                    pass
-    if caller == "unknown":
-        print "ERROR: unable to determine variant caller for file [{0}]".format(reader)
-        unknown_callers += 1
-    
-    return caller, unknown_callers, mutect_dict
-
 def determine_caller_and_split_mult_alts(reader, writer, unknown_callers):
     caller = "unknown"
     mutect_dict = {}
@@ -522,29 +502,19 @@ def process_files(sample_file_readers, input_dir, output_path, input_keys, heade
     for sample_file in sample_file_readers:
 #         print "{0} - reading ({1}/{2}): {3}".format(datetime.fromtimestamp(time.time()).strftime('%Y/%m/%d %H:%M:%S'), count + 1, len(sample_file_readers), sample_file)
         fname, extension = os.path.splitext(os.path.basename(sample_file))
-        if re.search(".splitMultAlts", fname):
-            reader = open(sample_file, "r")
-            caller, unknown_callers, mutect_dict = determine_caller(reader, unknown_callers)
-            reader.close()
-            
-            sample_columns = pivoter.add_file(sample_file, headers[count], caller, mutect_dict)
-            count += 1
-            
-            all_merge_context, all_merge_column_context = determine_merge_execution_context(all_merge_context, all_merge_column_context, sample_columns, sample_file, count)
+
+        new_sample_file = os.path.join(new_dir, fname + ".splitMultAlts" + extension)
         
-        else:
-            new_sample_file = os.path.join(new_dir, fname + ".splitMultAlts" + extension)
-            
-            reader = open(sample_file, "r")
-            writer = open(new_sample_file, "w")
-            caller, unknown_callers, mutect_dict = determine_caller_and_split_mult_alts(reader, writer, unknown_callers)
-            reader.close()
-            writer.close()
-    
-            sample_columns = pivoter.add_file(new_sample_file, headers[count], caller, mutect_dict)
-            count += 1
-            
-            all_merge_context, all_merge_column_context = determine_merge_execution_context(all_merge_context, all_merge_column_context, sample_columns, new_sample_file, count)
+        reader = open(sample_file, "r")
+        writer = open(new_sample_file, "w")
+        caller, unknown_callers, mutect_dict = determine_caller_and_split_mult_alts(reader, writer, unknown_callers)
+        reader.close()
+        writer.close()
+
+        sample_columns = pivoter.add_file(new_sample_file, headers[count], caller, mutect_dict)
+        count += 1
+        
+        all_merge_context, all_merge_column_context = determine_merge_execution_context(all_merge_context, all_merge_column_context, sample_columns, new_sample_file, count)
     
     if unknown_callers != 0:
         print "ERROR: unable to determine variant caller for [{0}] input files. Run (jacquard tag) first.".format(unknown_callers)
