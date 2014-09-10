16cd6f3c3924152be594c0462a394aceabf970e1
Jessica Bene <jebene@umich.edu>
2014-09-10 09:44:28 -0400

    EX-60 (cgates,jebene): Tweak to make Jacquard launcher test work in both *nix and windows

--- test/launch_jacquard_test.py
+++ test/launch_jacquard_test.py
@@ -6,6 +6,6 @@ TEST_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 class LaunchJacquardTest(unittest.TestCase):
     def test_launchJacquard(self):
-        p = subprocess.check_output([TEST_DIRECTORY + "/../bin/jacquard", "tag", "--help"], shell=True)
-        self.assertTrue("usage: jacquard.py tag [-h] input_dir output_dir" in p)
+        p = subprocess.check_output(TEST_DIRECTORY + '/../bin/jacquard tag --help', shell=True)
+        self.assertIn("usage: jacquard.py tag [-h] input_dir output_dir", p)
         
