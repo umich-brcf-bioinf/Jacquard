92c313b753c020e780ef3baa901b55e98b72c913
Chris Gates <cgates@umich.edu>
2014-09-22 10:12:05 -0400

    (cgates): added lightweight wrapper to run directly from source tree

---
+++ jacquard-runner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""Convenience wrapper for running jacquard directly from source tree."""
+
+
+from jacquard.jacquard import main
+
+
+if __name__ == '__main__':
+    main()
