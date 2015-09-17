2861c8d80b9304dbc46dd809ca452946532e8765
jebene <jebene@umich.edu>
2015-09-17 13:41:17 -0400

    ex-199 (jebene) minor change to conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -114,14 +114,14 @@ pygments_style = 'sphinx'
 todo_include_todos = True
 
 
-# -- Options for HTML output ----------------------------------------------
+# -- Options for HTML output ----------------------------------- -----------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
 
 # html_style = '../../../css/jacquard_theme.css'
-html_style = 'css/jacquard_theme.css'
+html_style = 'css/theme.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
