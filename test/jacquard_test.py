86abb2a7c31aea32b485b6b369354b3c9863be3d
cgates <cgates@umich.edu>
2018-06-06 13:11:57 -0400

    jq-320 (cgates): switch to semantic versioning

--- test/jacquard_test.py
+++ test/jacquard_test.py
@@ -55,7 +55,7 @@ class JacquardArgumentParserTestCase(test_case.JacquardBaseTestCase):
 
 class JacquardTestCase(test_case.JacquardBaseTestCase):
     def test_version(self):
-        self.assertEquals("0.43", jacquard.__version__)
+        self.assertEquals("1.0.0", jacquard.__version__)
 
     def test_get_execution_context(self):
         command = "foo input_dir output_dir"
